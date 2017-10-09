627beed534d3e8832c1b28ceec8d8d86c6d06a42
Steve Fisher <stevof@yahoo.com>
2017-10-09 12:30:16 -0400

    bonus requirements

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -10,15 +10,35 @@
   function LunchCheckController($scope) {
     $scope.dishes = '';
     $scope.message = '';
+    $scope.inputStyle = '';
+    $scope.messageStyle = '';
 
     $scope.checkDishes = function() {
       if ($scope.dishes) {
-        var dishesList = $scope.dishes.split(',');
-        dishesList = dishesList.filter(entry => entry.trim().length > 0);
-        $scope.message = dishesList.length <=3 ? 'Enjoy!' : 'Too much!';
+        var numDishes = getDishesCount($scope.dishes);
+
+        if (numDishes <=3) {
+          $scope.message = 'Enjoy!';
+          setStyles('valid');
+        } else {
+          $scope.message = 'Too much!';
+          setStyles('invalid');
+        }
       } else {
         $scope.message = 'Please enter data first';
+        setStyles('invalid');
       }
     };
+
+    function getDishesCount(dishesText) {
+      var dishesList = dishesText.split(',');
+      dishesList = dishesList.filter(entry => entry.trim().length > 0);
+      return dishesList.length
+    }
+
+    function setStyles(valid_invalid) {
+      $scope.inputStyle = 'input-' + valid_invalid;
+      $scope.messageStyle = 'message-' + valid_invalid;
+    }
   }
 })();
